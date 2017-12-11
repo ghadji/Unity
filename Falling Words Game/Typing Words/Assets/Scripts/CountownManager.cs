b622f55879ade4fe118424ac0481a23b69695580
ghadji <georgehadjidemetriou90@gmail.com>
2017-12-11 17:34:25 +0200

    v1.5
    Added menu scene with instructions and countdown screen

---
+++ Falling Words Game/Typing Words/Assets/Scripts/CountownManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CountownManager : MonoBehaviour {
+
+    public Text countdownText;
+    private float countdown = 3f;
+
+    void Update() {
+
+        if (countdown > 0) {
+            countdown -= Time.deltaTime;
+            countdownText.text = Mathf.RoundToInt(countdown).ToString();
+        } else {
+            SceneManager.LoadScene(1);
+        }
+
+    }
+
+}
---
+++ Falling Words Game/Typing Words/Assets/Scripts/StartGameManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGameManager : MonoBehaviour {
+
+    public GameObject countdownPanel;
+
+    public void startCountdown() {
+        countdownPanel.SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+
+}
