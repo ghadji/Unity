0bcf57f9341eb22e73bd42365f245c3a13c99153
ghadji <georgehadjidemetriou90@gmail.com>
2017-12-28 05:10:42 +0200

    v1
    - Initial project setup
    - Basic main menu scene created
    - Basic player model added
    - Basic movement functionality added
    - Camera functionality added
    - Scene fading added

---
+++ Don't You Push Me/Assets/Scripts/MainMenu/MainMenuController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour {
+
+    public SceneFader sceneFader;
+    public string levelToLoad = "MainScene";
+
+    public void StartGame() {
+        sceneFader.FadeTo(levelToLoad);
+    }
+
+    public void ExitGame() {
+        Application.Quit();
+    }
+}
