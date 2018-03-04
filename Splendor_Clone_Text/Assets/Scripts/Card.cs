6312057a0978b65231e028e1e26e8f2158d8c287
ghadji <georgehadjidemetriou90@gmail.com>
2018-03-04 02:13:57 +0200

    Card class rename

---
+++ Splendor_Clone_Text/Assets/Scripts/Card.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Cards/Card")]
+public class Card : ScriptableObject {
+
+    [Header("Points")]
+    public int points;
+
+    [Header("Cost")]
+    public int whiteGemCost;
+    public int redGemCost;
+    public int greenGemCost;
+    public int blueGemCost;
+    public int blackGemCost;
+
+
+}
