6c7aebc30a02655579407c0695393f512a0b33f7
ghadji <georgehadjidemetriou90@gmail.com>
2018-06-12 18:20:53 +0300

    Pong
    - Added UI score
    - Added ball respawning

--- Pong/Assets/Scripts/Ball.cs
+++ Pong/Assets/Scripts/Ball.cs
@@ -9,15 +9,19 @@ public class Ball : MonoBehaviour {
     float radius;
     Vector2 direction;
 
-	// Use this for initialization
-	void Start () {
-        direction = Vector2.one.normalized;
+    bool respawning;
+
+    // Use this for initialization
+    void Start() {
+        respawning = false;
+        LaunchBall();
         radius = transform.localScale.x / 2;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (!respawning)
+            transform.Translate(direction * speed * Time.deltaTime);
 
         //bounce on top and bottom of screen
         if (transform.position.y < GameManager.bottomLeft.y + radius && direction.y < 0) {
@@ -27,19 +31,47 @@ public class Ball : MonoBehaviour {
         if (transform.position.y > GameManager.topRight.y - radius && direction.y > 0) {
             direction.y = -direction.y;
         }
-
-        if (transform.position.x < GameManager.bottomLeft.x + radius && direction.x < 0) {
-            Debug.Log("Right Player won!");
-        }
-
-        if (transform.position.x > GameManager.topRight.x - radius && direction.x > 0) {
-            Debug.Log("Left Player won!");
-        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.tag == "Paddle") {
             direction.x = -direction.x;
         }
+        if (collision.tag == "GoalLeft") {
+            GameManager.Instance.IncreaseScoreRight();
+            StartCoroutine(ResetBall());
+        }
+        if (collision.tag == "GoalRight") {
+            GameManager.Instance.IncreaseScoreLeft();
+            StartCoroutine(ResetBall());
+        }
+    }
+
+    IEnumerator ResetBall() {
+        respawning = true;
+        transform.Translate(Vector3.zero);
+        transform.position = Vector2.zero;
+
+        yield return new WaitForSeconds(2f);
+
+        LaunchBall();
+    }
+
+    void LaunchBall() {
+        int randDir = Random.Range(0, 2);
+        int randCurve = Random.Range(0, 2);
+        if (randDir == 0) {
+            if (randCurve == 0)
+                direction = Vector2.one.normalized;
+            else
+                direction = new Vector2(1, -1).normalized;
+        } else {
+            direction = -Vector2.one.normalized;
+            if (randCurve == 0)
+                direction = new Vector2(-1, 1).normalized;
+            else
+                direction = -Vector2.one.normalized;
+        }
+        respawning = false;
     }
 }
--- Pong/Assets/Scripts/GameManager.cs
+++ Pong/Assets/Scripts/GameManager.cs
@@ -1,17 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
+    public static GameManager Instance { get; set; }
     public Ball ball;
     public Paddle paddle;
 
     public static Vector2 bottomLeft;
     public static Vector2 topRight;
 
+    int scoreLeft;
+    int scoreRight;
+    public Text scoreLeftText;
+    public Text scoreRightText;
+
     // Use this for initialization
     void Start () {
+        Instance = this;
+
+        scoreLeft = 0;
+        scoreRight = 0;
+
+        scoreLeftText.text = scoreLeft.ToString();
+        scoreRightText.text = scoreRight.ToString();
+
         //convert screen's pixel coordinate into game's coordinate
         bottomLeft = Camera.main.ScreenToWorldPoint(new Vector2(0, 0));
 
@@ -26,4 +41,14 @@ public class GameManager : MonoBehaviour {
         paddle1.Init(true);
         paddle2.Init(false);
 	}
+
+    public void IncreaseScoreLeft() {
+        scoreLeft++;
+        scoreLeftText.text = scoreLeft.ToString();
+    }
+
+    public void IncreaseScoreRight() {
+        scoreRight++;
+        scoreRightText.text = scoreRight.ToString();
+    }
 }
